ee385d754a88353aeb5f48aaea326f268b0a5f00
ayubero <andreees.ys@gmail.com>
2023-10-10 15:50:06 +0200

    database

---
+++ vos/component.js
@@ -0,0 +1,10 @@
+class Component {
+    constructor(code, name, quantity, price) {
+        this.code = code;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+}
+
+module.exports = Component;
